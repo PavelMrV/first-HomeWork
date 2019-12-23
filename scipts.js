8956f07781e1100622ee470f920a57812871afe0
Pavel <miron21sb@gmail.com>
2019-12-23 15:47:20 +0300

    second hw

--- scipts.js
+++ scipts.js
@@ -1,4 +1,4 @@
-let money = prompt("Ваш бюджет на месяц?", '');
+let money = +prompt("Ваш бюджет на месяц?", '');
 let time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
 let appData = {
@@ -11,13 +11,74 @@ let appData = {
 
 };
 
-let answer1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    answer2 = prompt("Во сколько обойдется?", ''),
-    answer3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    answer4 = prompt("Во сколько обойдется?", '');
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
+
+    if ((typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+        } else {
+        console.log("bad result");
+        i--;
+
+}
+
+// Используем цикл WHILE
+
+// let i = 0;
+// while (i < 2) {
+//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt ("Во сколько обойдется?", "");
+
+//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+//         console.log ("done");
+
+//         appData.expenses[a] = b;
+//     } else {
+//          console.log ("bad result");
+//          i--;
+//     }
+
+//     i++;
+// }
+
+// Используем цикл DO...WHILE
+
+// let i = 0;
+// do {
+//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt ("Во сколько обойдется?", "");
+
+//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+//         console.log ("done");
+
+//         appData.expenses[a] = b;
+//     } else {
+//          console.log ("bad result");
+//          i--;
+//     }
+
+//     i++;
+// }
+// while(i < 2);
 
 
-appData.expenses[answer1] = answer2;
-appData.expenses[answer3] = answer4;
+};
 
-alert(appData.budget/30);
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+        console.log("Минимальный уровень достатка")
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log("Средний уровень достатка");
+    } else if (appData.moneyPerDay > 2000) {
+        console.log("Высокий уровень достатка");
+    } else {
+        console.log("Произошла ошибка")
+    }
